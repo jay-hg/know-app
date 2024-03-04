face7ab35fd3ed193304a6568669efbee7e1d38a
acai <1175867101@qq.com>
2024-03-04 11:49:10 +0800

    加票按钮

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -2,13 +2,18 @@
   <div>
     <div class="header">
       <div style="display: flex">
-        <van-icon @click="handleBack" size="23" class="left-icon" name="arrow-left"/>
+        <van-icon
+          @click="handleBack"
+          size="23"
+          class="left-icon"
+          name="arrow-left"
+        />
         <van-image
-            class="avatar"
-            round
-            width="37"
-            height="37"
-            :src="base + '/file/image/' + data.author.avatar"
+          class="avatar"
+          round
+          width="37"
+          height="37"
+          :src="base + '/file/image/' + data.author.avatar"
         />
         <div class="des">
           <div class="des-name">{{ data.author.nickname }}</div>
@@ -18,7 +23,7 @@
         </div>
       </div>
       <div class="follow" @click="showShare = true">
-        <van-icon name="share"/>
+        <van-icon name="share" />
         分享
       </div>
     </div>
@@ -26,46 +31,75 @@
       <div class="content-title">
         {{ data.article.title }}
       </div>
-      <van-divider/>
+      <van-divider />
       <div class="content-main">
-        <MarkdownContent :content="content"/>
+        <MarkdownContent :content="content" />
       </div>
-      <div class="content-time">发布于 {{ data.article.createAt }} · 未经作者允许禁止转载</div>
+      <div class="content-time">
+        发布于 {{ data.article.createAt }} · 未经作者允许禁止转载
+      </div>
     </div>
 
     <div class="bottom-bar">
-      <van-button :loading="agreeLoading"
-                  loading-text="加载中..."
-                  round type="info"
-                  @click="agree">
+      <van-button
+        :loading="agreeLoading"
+        loading-text="加载中..."
+        round
+        type="info"
+        @click="agree"
+      >
         <div style="display: flex">
-          <van-icon class="bottom-bar-icon" size="20" name="play"/>
-          <span class="s1">赞同 {{ data.article.agreeCount }}</span>
+          <van-icon class="bottom-bar-icon" size="20" name="play" />
+          <span class="s1">投票 {{ data.article.agreeCount }}</span>
+        </div>
+      </van-button>
+      <van-button
+        round
+        type="info"
+        :to="'/article/gift-vote/' + this.$route.params.id"
+      >
+        <div style="display: flex">
+          <van-icon class="bottom-bar-icon" size="20" name="play" />
+          <span class="s1">加票</span>
         </div>
       </van-button>
       <a @click="show = true" style="float: right">
         <div class="comments-count">{{ data.article.commentCount }}条评论</div>
-        <van-icon color="#909399" size="25" class="comments-icon" name="chat-o"/>
+        <van-icon
+          color="#909399"
+          size="25"
+          class="comments-icon"
+          name="chat-o"
+        />
       </a>
     </div>
-    <van-popup closeable
-               close-icon="clear"
-               v-model="show"
-               round
-               position="bottom"
-               :style="{ height: '90%' }">
+    <van-popup
+      closeable
+      close-icon="clear"
+      v-model="show"
+      round
+      position="bottom"
+      :style="{ height: '90%' }"
+    >
       <div style="padding: 15px">
         <div class="popup-title">全部评论</div>
         <div class="popup-content">
-          <div style="padding-top: 10px;letter-spacing: 1px;padding-bottom: 20px">评论 {{ commentList.length }}
+          <div
+            style="padding-top: 10px; letter-spacing: 1px; padding-bottom: 20px"
+          >
+            评论 {{ commentList.length }}
           </div>
-          <div v-for="(item, index) in commentList" :key="index" class="comments-item">
+          <div
+            v-for="(item, index) in commentList"
+            :key="index"
+            class="comments-item"
+          >
             <van-image
-                style="margin-top: 5px"
-                round
-                width="30"
-                height="30"
-                :src="base + '/file/image/' + item.user.avatar"
+              style="margin-top: 5px"
+              round
+              width="30"
+              height="30"
+              :src="base + '/file/image/' + item.user.avatar"
             />
             <div style="margin-left: 15px">
               <div class="comments-item-d1">{{ item.user.nickname }}</div>
@@ -76,7 +110,11 @@
         </div>
         <div class="popup-bottom">
           <label>
-            <input placeholder="发一条友善的评论" v-model="commentForm.content" class="bottom-bar-input"/>
+            <input
+              placeholder="发一条友善的评论"
+              v-model="commentForm.content"
+              class="bottom-bar-input"
+            />
           </label>
           <div class="popup-bottom-send" @click="submitComment">发布</div>
         </div>
@@ -84,38 +122,37 @@
     </van-popup>
 
     <van-share-sheet
-        v-model="showShare"
-        title="立即分享给好友"
-        :options="options"
+      v-model="showShare"
+      title="立即分享给好友"
+      :options="options"
     />
   </div>
 </template>
 
 <script>
 import MarkdownContent from "@/components/MarkdownContent";
-import {Agree, FindArticleById} from "@/api/article";
-import {BASE_RUL} from "@/utils/request";
-import {FindAllComment, SaveComment} from "@/api/comment";
+import { Agree, FindArticleById } from "@/api/article";
+import { BASE_RUL } from "@/utils/request";
+import { FindAllComment, SaveComment } from "@/api/comment";
 
 export default {
-
-  components: {MarkdownContent},
+  components: { MarkdownContent },
 
   data() {
     return {
       showShare: false,
       options: [
-        {name: '微信', icon: 'wechat'},
-        {name: '微博', icon: 'weibo'},
-        {name: '复制链接', icon: 'link'},
-        {name: '分享海报', icon: 'poster'},
-        {name: '二维码', icon: 'qrcode'},
+        { name: "微信", icon: "wechat" },
+        { name: "微博", icon: "weibo" },
+        { name: "复制链接", icon: "link" },
+        { name: "分享海报", icon: "poster" },
+        { name: "二维码", icon: "qrcode" },
       ],
       agreeLoading: false,
       commentForm: {
         uid: localStorage.getItem("uid"),
         aid: this.$route.params.id,
-        content: ''
+        content: "",
       },
       commentList: [],
       base: BASE_RUL,
@@ -123,53 +160,50 @@ export default {
         article: {},
         author: {},
       },
-      content: '',
+      content: "",
       show: false,
-    }
+    };
   },
 
   mounted() {
     FindArticleById(this.$route.params.id).then((res) => {
-      if (res.status) this.data = res.data
-      this.content = this.data.article.content.toString()
-    })
+      if (res.status) this.data = res.data;
+      this.content = this.data.article.content.toString();
+    });
     FindAllComment(this.$route.params.id).then((res) => {
-      if (res.status) this.commentList = res.data
-    })
+      if (res.status) this.commentList = res.data;
+    });
   },
 
   methods: {
-
     handleBack() {
-      this.$router.back()
+      this.$router.back();
     },
 
     submitComment() {
       SaveComment(this.commentForm).then((res) => {
         if (res.status) {
-          this.commentList.push(res.data)
-          this.data.article.commentCount = this.data.article.commentCount + 1
-          this.$toast.success("感谢你的评论")
+          this.commentList.push(res.data);
+          this.data.article.commentCount = this.data.article.commentCount + 1;
+          this.$toast.success("感谢你的评论");
         }
-      })
+      });
     },
 
     agree() {
-      this.agreeLoading = true
+      this.agreeLoading = true;
       Agree(this.$route.params.id).then((res) => {
         if (res.status) {
-          this.data.article.agreeCount = this.data.article.agreeCount + 1
+          this.data.article.agreeCount = this.data.article.agreeCount + 1;
         }
         setTimeout(() => {
-          this.$toast.success("谢谢你的赞同")
-          this.agreeLoading = false
-        }, 700)
-      })
+          this.$toast.success("谢谢你的赞同");
+          this.agreeLoading = false;
+        }, 700);
+      });
     },
-
   },
-
-}
+};
 </script>
 
 <style scoped>
@@ -200,11 +234,12 @@ export default {
 
 .des-name {
   font-size: 15px;
-  color: rgba(0, 0, 0, .8);
+  color: rgba(0, 0, 0, 0.8);
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 1; /* 可以显示的行数，超出部分用...表示*/
+  -webkit-line-clamp: 1;
+  /* 可以显示的行数，超出部分用...表示*/
   -webkit-box-orient: vertical;
 }
 
@@ -212,11 +247,12 @@ export default {
   display: flex;
   padding-top: 5px;
   font-size: 12px;
-  color: rgba(0, 0, 0, .5);
+  color: rgba(0, 0, 0, 0.5);
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 1; /* 可以显示的行数，超出部分用...表示*/
+  -webkit-line-clamp: 1;
+  /* 可以显示的行数，超出部分用...表示*/
   -webkit-box-orient: vertical;
 }
 
@@ -242,12 +278,11 @@ export default {
 }
 
 .content-main {
-
 }
 
 .content-time {
   font-size: 13px;
-  color: rgba(0, 0, 0, .4);
+  color: rgba(0, 0, 0, 0.4);
   letter-spacing: 1px;
   padding: 20px 0;
 }
@@ -328,7 +363,7 @@ export default {
   height: 40px;
   font-size: 13px;
   letter-spacing: 1px;
-  color: rgba(0, 0, 0, .4);
+  color: rgba(0, 0, 0, 0.4);
   margin-top: 13px;
   margin-left: 20px;
   width: 70%;
@@ -344,17 +379,16 @@ export default {
 }
 
 .comments-item-d1 {
-  color: rgba(0, 0, 0, .9);
+  color: rgba(0, 0, 0, 0.9);
 }
 
 .comments-item-d2 {
-  color: rgba(0, 0, 0, .7);
+  color: rgba(0, 0, 0, 0.7);
   padding: 4px 0;
 }
 
 .comments-item-d3 {
-  color: rgba(0, 0, 0, .4);
+  color: rgba(0, 0, 0, 0.4);
   font-size: 11px;
 }
-
 </style>
